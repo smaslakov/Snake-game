80911954b4e13d3858f4bac7f434fb778268cb44
smaslakov <stasmaslakov2006@gmail.com>
2024-05-13 23:49:10 +0300

    db

---
+++ database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stas Maslakov on 13.05.2024.
+//
+
+#include "database.h"
---
+++ database.h
@@ -0,0 +1,16 @@
+//
+// Created by Stas Maslakov on 13.05.2024.
+//
+
+#ifndef SNAKE_GAME_DATABASE_H
+#define SNAKE_GAME_DATABASE_H
+
+
+
+class database {
+
+};
+
+
+
+#endif //SNAKE_GAME_DATABASE_H
